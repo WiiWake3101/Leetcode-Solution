0de357b3affc8a86063358196da8ccb64faea131
Vivek M G <113228802+WiiWake3101@users.noreply.github.com>
2023-11-19 06:11:29 +0530

    Time: 920 ms (15.98%) | Memory: 27.5 MB (30.41%) - LeetSync

--- 2016-reduction-operations-to-make-the-array-elements-equal/reduction-operations-to-make-the-array-elements-equal.py
+++ 2016-reduction-operations-to-make-the-array-elements-equal/reduction-operations-to-make-the-array-elements-equal.py
@@ -1,10 +1,10 @@
+from collections import Counter
 class Solution:
     def reductionOperations(self, nums: List[int]) -> int:
-        nums.sort()
-        ans=0
-        up=0
-        for i in range (1,len(nums)):
-            if nums[i]!=nums[i-1]:
-                up+=1
-            ans+=up
+        count = Counter(nums)
+        keys = sorted(count.keys(), reverse=True)
+        ans = 0
+        for i in range(1, len(keys)):
+            count[keys[i]] += count[keys[i-1]]
+            ans += count[keys[i-1]]
         return ans
