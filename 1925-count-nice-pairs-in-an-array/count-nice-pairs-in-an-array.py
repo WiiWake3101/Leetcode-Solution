ae289f14e848cfe6196fdad554a7cd275e8f0959
Vivek M G <113228802+WiiWake3101@users.noreply.github.com>
2023-11-21 08:13:24 +0530

    Time: 654 ms (25.78%) | Memory: 26.9 MB (21.53%) - LeetSync

--- 1925-count-nice-pairs-in-an-array/count-nice-pairs-in-an-array.py
+++ 1925-count-nice-pairs-in-an-array/count-nice-pairs-in-an-array.py
@@ -5,17 +5,13 @@ class Solution:
             while num:
                 result = result * 10 + num % 10
                 num //= 10
-            
             return result
 
-        arr = []
-        for i in range(len(nums)):
-            arr.append(nums[i] - rev(nums[i]))
-        
         dic = defaultdict(int)
         ans = 0
         MOD = 10 ** 9 + 7
-        for num in arr:
+        for num in nums:
+            num -= rev(num)
             ans = (ans + dic[num]) % MOD
             dic[num] += 1
         
