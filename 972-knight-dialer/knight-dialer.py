008a8af954cbb05b6cb4ec1a5b199fd5a0ede579
Vivek M G <113228802+WiiWake3101@users.noreply.github.com>
2023-11-27 22:22:28 +0530

    Time: 60 ms (99.45%) | Memory: 16.4 MB (73.06%) - LeetSync

--- 972-knight-dialer/knight-dialer.py
+++ 972-knight-dialer/knight-dialer.py
@@ -1,32 +1,33 @@
+graph = [
+    [4,6],
+    [6,8],
+    [7,9],
+    [4,8],
+    [0,3,9],
+    [],
+    [0,1,7],
+    [2,6],
+    [1,3],
+    [2,4],
+]
+
+MOD = 10**9+7
+
+DP = [0, 10]
+state = [1] * 10
+nxt_state = [0] * 10
+
 class Solution:
     def knightDialer(self, n: int) -> int:
-        @cache
-        def dp(remain, square):
-            if remain == 0:
-                return 1
-            
-            ans = 0
-            for next_square in jumps[square]:
-                ans = (ans + dp(remain - 1, next_square)) % MOD
-            
-            return ans
-        
-        jumps = [
-            [4, 6],
-            [6, 8],
-            [7, 9],
-            [4, 8],
-            [3, 9, 0],
-            [],
-            [1, 7, 0],
-            [2, 6],
-            [1, 3],
-            [2, 4]
-        ]
+        global state, nxt_state
+        for _ in range(len(DP), n+1):
+            nxt_state[:] = [0] * 10
+            for cur, ct in enumerate(state):
+                for nxt in graph[cur]:
+                    nxt_state[nxt] += ct
+            for i in range(10):
+                nxt_state[i] %= MOD
 
-        ans = 0
-        MOD = 10 ** 9 + 7
-        for square in range(10):
-            ans = (ans + dp(n - 1, square)) % MOD
-        
-        return ans
+            DP.append( sum(nxt_state) % MOD )
+            state,nxt_state = nxt_state,state
+        return DP[n]
