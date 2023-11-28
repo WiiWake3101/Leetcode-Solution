49a0eb5649c3faf22d4a1dc1394e54c1ea69b20d
Vivek M G <113228802+WiiWake3101@users.noreply.github.com>
2023-11-28 21:12:12 +0530

    Time: 377 ms (75.71%) | Memory: 17.4 MB (71.43%) - LeetSync

--- 2251-number-of-ways-to-divide-a-long-corridor/number-of-ways-to-divide-a-long-corridor.py
+++ 2251-number-of-ways-to-divide-a-long-corridor/number-of-ways-to-divide-a-long-corridor.py
@@ -1,31 +1,37 @@
 class Solution:
     def numberOfWays(self, corridor: str) -> int:
-        cntseat = 0
-        mod = 1000000007
+        # Store 1000000007 in a variable for convenience
+        MOD = 1_000_000_007
 
-        for ch in corridor:
-            if ch == 'S':
-                cntseat += 1
+        # Total number of ways
+        count = 1
 
-        if cntseat % 2 or cntseat == 0:
+        # Number of seats in current section
+        seats = 0
+
+        # Tracking Index of last S in the previous section
+        previous_pair_last = None
+
+        # Keep track of seats in the corridor
+        for index, thing in enumerate(corridor):
+            if thing == "S":
+                seats += 1
+
+                # If two seats, then this is the last S in the section
+                # Update seats for the next section
+                if seats == 2:
+                    previous_pair_last = index
+                    seats = 0
+                
+                # If one seat, then this is the first S in the section
+                # Compute the product of non-paired neighbors
+                elif seats == 1 and previous_pair_last is not None:
+                    count *= (index - previous_pair_last)
+                    count %= MOD
+        
+        # If odd seats, or zero seats
+        if seats == 1 or previous_pair_last is None:
             return 0
 
-        # Do not put any divider
-        if cntseat == 2:
-            return 1
-
-        cnt = 0
-        last = 0
-        res = 1
-
-        for i in range(len(corridor)):
-            if corridor[i] == 'S':
-                cnt += 1
-                if cnt % 2 == 0:
-                    last = i
-                if cnt % 2 != 0 and cnt != 1:
-                    res *= (i - last)
-                    res %= mod
-
-        return res
-        
+        # Return the number of ways
+        return count
