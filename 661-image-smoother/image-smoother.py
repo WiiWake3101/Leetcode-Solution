aca54e3196dc39bdc6d463067aa64bd328ce4139
Vivek M G <113228802+WiiWake3101@users.noreply.github.com>
2023-12-19 14:39:41 +0530

    Time: 346 ms (95.95%) | Memory: 13.9 MB (36.49%) - LeetSync

--- 661-image-smoother/image-smoother.py
+++ 661-image-smoother/image-smoother.py
@@ -1,27 +1,39 @@
 class Solution(object):
     def imageSmoother(self, img):
         """
-        :type img: List[List[int]]
+        :type M: List[List[int]]
         :rtype: List[List[int]]
         """
-        m, n = len(img), len(img[0])
-        res = [[0] * n for _ in range(m)]
-
+        m = len(img)
+        n = len(img[0])
+        result = [[i for i in row] for row in img]
         for i in range(m):
             for j in range(n):
-                res[i][j] = self.smoothen(img, i, j)
-
-        return res
-
-    def smoothen(self, img, x, y):
-        m, n = len(img), len(img[0])
-        _sum, count = 0, 0
-
-        for i in range(-1, 2):
-            for j in range(-1, 2):
-                nx, ny = x + i, y + j
-                if 0 <= nx < m and 0 <= ny < n:
-                    _sum += img[nx][ny]
+                total = img[i][j]
+                count = 1
+                if i > 0:
+                    total += img[i-1][j]
                     count += 1
-
-        return _sum // count        
+                if j > 0:
+                    total += img[i][j - 1]
+                    count += 1
+                if i < m - 1:
+                    total += img[i+1][j]
+                    count += 1
+                if j < n - 1:
+                    total += img[i][j+1]
+                    count += 1
+                if i > 0 and j > 0:
+                    total += img[i-1][j-1]
+                    count += 1
+                if i < m - 1 and j < n - 1:
+                    total += img[i+1][j+1]
+                    count += 1
+                if i > 0 and j < n - 1:
+                    total += img[i-1][j+1]
+                    count += 1
+                if i < m - 1 and j > 0:
+                    total += img[i+1][j-1]
+                    count += 1
+                result[i][j] = total / count
+        return result  
