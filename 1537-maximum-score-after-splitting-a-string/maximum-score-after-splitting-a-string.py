f69b11ea8f4b6a50612b20ce47584b9f4aedcbb4
Vivek M G <113228802+WiiWake3101@users.noreply.github.com>
2023-12-22 07:54:32 +0530

    Time: 146 ms (8.33%) | Memory: 13.3 MB (56.94%) - LeetSync

---
+++ 1537-maximum-score-after-splitting-a-string/maximum-score-after-splitting-a-string.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    def maxScore(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        ans = 0
+        
+        for i in range(len(s) - 1):
+            curr = 0
+            for j in range(i + 1):
+                if s[j] == "0":
+                    curr += 1
+            
+            for j in range(i + 1, len(s)):
+                if s[j] == "1":
+                    curr += 1
+            
+            ans = max(ans, curr)
+    
+        return ans
