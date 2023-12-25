9bccc1f0d2e2ba9f663332e9e2d33912a41b29f1
Vivek M G <113228802+WiiWake3101@users.noreply.github.com>
2023-12-25 07:39:39 +0530

    Time: 16 ms (57.22%) | Memory: 13.7 MB (20.76%) - LeetSync

--- 91-decode-ways/decode-ways.py
+++ 91-decode-ways/decode-ways.py
@@ -1,25 +1,38 @@
 class Solution(object):
     def numDecodings(self, s):
+
         """
         :type s: str
         :rtype: int
         """
-        if not s or s[0] == '0':
+
+        if s[0] == "0":
             return 0
-
-        n = len(s)
-        dp = [0] * (n + 1)
-        dp[0] = 1
-        dp[1] = 1
-
-        for i in range(2, n + 1):
-            one_digit = int(s[i - 1])
-            two_digits = int(s[i - 2:i])
-
-            if one_digit != 0:
-                dp[i] += dp[i - 1]
-
-            if 10 <= two_digits <= 26:
-                dp[i] += dp[i - 2]
-
-        return dp[n]
+        if len(s) == 1:
+            return 1
+        if int(s[-1]) == 0:
+            if int(s[-2]) not in [1, 2]:    #not 10, 20
+                return 0
+            else:                           #10, 20
+                next1, next2 = 1, 0
+        elif int(s[-2]) == 0:
+            next1, next2 = 0, 1
+        elif 10 < int(s[-2:]) < 27:
+            next1, next2 = 2, 1
+        else:
+            next1, next2 = 1, 1
+        i = len(s) - 3
+        while i > -1:
+            temp = 0
+            if 9 < int(s[i:i+2]) < 27:
+                temp = 1
+            if int(s[i]) == 0:
+                curr = 0
+            else:
+                curr = next1 + temp*next2
+            next1, next2 = curr, next1
+            i -= 1
+        if len(s) == 2:
+            return next1
+        return curr
+    
