2097097b24092db946d0adf9bdc47d32cbe5793d
Vivek M G <113228802+WiiWake3101@users.noreply.github.com>
2024-01-05 06:51:32 +0530

    Time: 47 ms (93.81%) | Memory: 13.6 MB (84.30%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.py
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.py
@@ -4,15 +4,13 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        if not nums:
-            return 0
-        
-        n = len(nums)
-        dp = [1] * n
+        ans = [nums[0]]
 
-        for i in range(1, n):
-            for j in range(i):
-                if nums[i] > nums[j]:
-                    dp[i] = max(dp[i], dp[j] + 1)
+        for i in nums:
+            if ans[-1] < i:
+                ans.append(i)
+            else:
+                seq= bisect.bisect_left(ans,i)
+                ans[seq] = i
 
-        return max(dp)
+        return len(ans) 
