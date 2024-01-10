46b0f9fda2e9db531b3e44a8ecc10d887eba275b
Vivek M G <113228802+WiiWake3101@users.noreply.github.com>
2024-01-10 06:49:44 +0530

    Time: 525 ms (26.54%) | Memory: 186.1 MB (24.96%) - LeetSync

--- 2461-amount-of-time-for-binary-tree-to-be-infected/amount-of-time-for-binary-tree-to-be-infected.cpp
+++ 2461-amount-of-time-for-binary-tree-to-be-infected/amount-of-time-for-binary-tree-to-be-infected.cpp
@@ -10,56 +10,50 @@
  * };
  */
 class Solution {
-    void parentp(TreeNode* root, map<TreeNode*, TreeNode*>& mp) {
-        if (root == NULL) {
-            return;
-        }
-        if (root->left != NULL) {
-            mp[root->left] = root;
-            parentp(root->left, mp);
-        }
-        if (root->right != NULL) {
-            mp[root->right] = root;
-            parentp(root->right, mp); // Corrected this line
-        }
-        return;
-    }
-
-    void findnode(TreeNode* root, int start, TreeNode*& x) {
-        if (root == NULL) {
-            return;
-        }
-        if (root->val == start) {
-            x = root;
-            return;
-        }
-        findnode(root->left, start, x);
-        findnode(root->right, start, x);
-    }
-
-    void dfs(TreeNode* x, map<TreeNode*, TreeNode*>& mp, set<TreeNode*>& vis, int min, int& result) {
-        if (x == NULL) {
-            result = max(result, min - 1);
-            return;
-        }
-        if (vis.find(x) != vis.end()) {
-            return;
-        }
-        vis.insert(x);
-        dfs(x->left, mp, vis, min + 1, result);
-        dfs(x->right, mp, vis, min + 1, result);
-        dfs(mp[x], mp, vis, min + 1, result);
-    }
-
 public:
+    unordered_map<int, vector<int>> graph;
+
     int amountOfTime(TreeNode* root, int start) {
-        map<TreeNode*, TreeNode*> mp; // to store the parent
-        parentp(root, mp);
-        TreeNode* x = NULL;
-        findnode(root, start, x);
-        set<TreeNode*> vis;
-        int result = 0;
-        dfs(x, mp, vis, 0, result);
-        return result;
+        constructGraph(root);
+
+        queue<int> q;
+        q.push(start);
+
+        unordered_set<int> visited;
+
+        int minutesPassed = -1;
+
+        while (!q.empty()) {
+            ++minutesPassed;
+            for (int levelSize = q.size(); levelSize > 0; --levelSize) {
+                int currentNode = q.front();
+                q.pop();
+                visited.insert(currentNode);
+                for (int adjacentNode : graph[currentNode]) {
+                    if (!visited.count(adjacentNode)) {
+                        q.push(adjacentNode);
+                    }
+                }
+            }
+        }
+
+        return minutesPassed;
     }
-}; 
+
+    void constructGraph(TreeNode* root) {
+        if (!root) return;
+
+        if (root->left) {
+            graph[root->val].push_back(root->left->val);
+            graph[root->left->val].push_back(root->val);
+        }
+
+        if (root->right) {
+            graph[root->val].push_back(root->right->val);
+            graph[root->right->val].push_back(root->val);
+        }
+
+        constructGraph(root->left);
+        constructGraph(root->right);
+    }
+};
