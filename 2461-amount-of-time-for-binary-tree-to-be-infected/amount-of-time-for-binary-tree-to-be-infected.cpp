0e520f465efdf45c555ca589d4fc64502af076bc
Vivek M G <113228802+WiiWake3101@users.noreply.github.com>
2024-01-10 06:50:14 +0530

    Time: 148 ms (90.77%) | Memory: 123.3 MB (90.42%) - LeetSync

--- 2461-amount-of-time-for-binary-tree-to-be-infected/amount-of-time-for-binary-tree-to-be-infected.cpp
+++ 2461-amount-of-time-for-binary-tree-to-be-infected/amount-of-time-for-binary-tree-to-be-infected.cpp
@@ -9,51 +9,59 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+const int N = 100001;
+vector<int>adj[N];
 class Solution {
-public:
-    unordered_map<int, vector<int>> graph;
+    public:
+      struct TraverseDists {
+        int distToDisease = 0;
+        int length = 0;
+    };
 
     int amountOfTime(TreeNode* root, int start) {
-        constructGraph(root);
-
-        queue<int> q;
-        q.push(start);
-
-        unordered_set<int> visited;
-
-        int minutesPassed = -1;
-
-        while (!q.empty()) {
-            ++minutesPassed;
-            for (int levelSize = q.size(); levelSize > 0; --levelSize) {
-                int currentNode = q.front();
-                q.pop();
-                visited.insert(currentNode);
-                for (int adjacentNode : graph[currentNode]) {
-                    if (!visited.count(adjacentNode)) {
-                        q.push(adjacentNode);
-                    }
-                }
-            }
-        }
-
-        return minutesPassed;
+        int time = 0;
+        traverse(root, start, time);
+        return time;
     }
 
-    void constructGraph(TreeNode* root) {
-        if (!root) return;
+    TraverseDists traverse(TreeNode* root, int start, int& time) {
+        TraverseDists result;
+        if (!root)
+            return result;
 
-        if (root->left) {
-            graph[root->val].push_back(root->left->val);
-            graph[root->left->val].push_back(root->val);
+        // We traverse nodes from root to left subtree first
+        // If we find disease node in that subtree we need to return distance from root to disease node
+        // If we wont find disease node in that subtree than we need to find distance to longest leaf
+        TraverseDists left = traverse(root->left, start, time);
+        TraverseDists right = traverse(root->right, start, time);
+
+        // Current node is a node with disease
+        if (root->val == start) {
+            time = max(left.length, right.length);
+            result.distToDisease = 1;
+            return result;
         }
 
-        if (root->right) {
-            graph[root->val].push_back(root->right->val);
-            graph[root->right->val].push_back(root->val);
+        // We haven't found node with disease on any of child subtrees
+        // In that case we need to return distance to most far leaf from those subtrees
+        if (left.distToDisease == 0 && right.distToDisease == 0) {
+            result.length = max(left.length, right.length) + 1;
+            return result;
         }
 
-        constructGraph(root->left);
-        constructGraph(root->right);
+        // One of subtrees has disease
+        if (left.distToDisease > 0) {
+            time = max(time, right.length + left.distToDisease);
+            result.distToDisease = left.distToDisease + 1;
+            return result;
+        }
+
+        if (right.distToDisease > 0) {
+            time = max(time, left.length + right.distToDisease);
+            result.distToDisease = right.distToDisease + 1;
+            return result;
+        }
+
+        return result;
     }
 };
