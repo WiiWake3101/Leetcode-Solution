a2fe6e24bd337415faeb988258d5f6e988b5a2d9
Vivek M G <113228802+WiiWake3101@users.noreply.github.com>
2024-01-13 08:08:06 +0530

    Time: 65 ms (98.57%) | Memory: 14.2 MB (57.14%) - LeetSync

--- 1469-minimum-number-of-steps-to-make-two-strings-anagram/minimum-number-of-steps-to-make-two-strings-anagram.py
+++ 1469-minimum-number-of-steps-to-make-two-strings-anagram/minimum-number-of-steps-to-make-two-strings-anagram.py
@@ -5,17 +5,11 @@ class Solution(object):
         :type t: str
         :rtype: int
         """
-        count = [0]*26
-        # Storing the difference of frequencies of characters in t and s.
-        for i in range(len(s)):
-            count[ord(t[i]) - ord('a')] += 1
-            count[ord(s[i]) - ord('a')] -= 1
+        res = 0
+        s_set = set(s)
 
-        ans = 0
-        # Adding the difference where string t has more instances than s.
-        # Ignoring where t has fewer instances as they are redundant and
-        # can be covered by the first case.
-        for i in range(26):
-            ans += max(0, count[i])
-        
-        return ans
+        for char in s_set:
+            diff = s.count(char) - t.count(char)
+            if diff > 0:
+                res += diff
+        return res
