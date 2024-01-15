61ae93bab18c29953bd1d4dd10f96ef8e7f7bfb3
Vivek M G <113228802+WiiWake3101@users.noreply.github.com>
2024-01-15 07:38:32 +0530

    Time: 2171 ms (8.95%) | Memory: 73 MB (68.87%) - LeetSync

---
+++ 1354-find-players-with-zero-or-one-losses/find-players-with-zero-or-one-losses.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    def findWinners(self, matches):
+        """
+        :type matches: List[List[int]]
+        :rtype: List[List[int]]
+        """
+        losses = [0] * 100001
+
+        for winner, loser in matches:
+            if losses[winner] == 0:
+                losses[winner] = -1
+
+            if losses[loser] == -1:
+                losses[loser] = 1
+            else:
+                losses[loser] += 1
+
+        zero_loss = [i for i in range(1, 100001) if losses[i] == -1]
+        one_loss = [i for i in range(1, 100001) if losses[i] == 1]
+
+        return [zero_loss, one_loss]
