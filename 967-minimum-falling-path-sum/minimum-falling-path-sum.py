9e0316aaceeb4c968a2915792d9b6158c2a8f6e2
Vivek M G <113228802+WiiWake3101@users.noreply.github.com>
2024-01-19 06:11:46 +0530

    Time: 85 ms (100.00%) | Memory: 14.2 MB (41.90%) - LeetSync

--- 967-minimum-falling-path-sum/minimum-falling-path-sum.py
+++ 967-minimum-falling-path-sum/minimum-falling-path-sum.py
@@ -4,26 +4,15 @@ class Solution(object):
         :type matrix: List[List[int]]
         :rtype: int
         """
-        n = len(matrix)
-        m = len(matrix[0])
-        dp = [[0] * m for _ in range(n)]
-
-        for j in range(m):
-            dp[0][j] = matrix[0][j]
-
-        for i in range(1, n):
-            for j in range(m):
-                ld = rd = float('inf')
-                up = matrix[i][j] + dp[i - 1][j]
-
-                if j - 1 >= 0:
-                    ld = matrix[i][j] + dp[i - 1][j - 1]
-                if j + 1 < m:
-                    rd = matrix[i][j] + dp[i - 1][j + 1]
-
-                dp[i][j] = min(up, min(ld, rd))
-
-        mini = dp[n - 1][0]
-        for j in range(1, m):
-            mini = min(mini, dp[n - 1][j])
-        return mini
+        m = len(matrix)
+        n = len(matrix[0])
+        for i in range(1,m):
+            for j in range(n):
+                if j+1 < m and j > 0:
+                    matrix[i][j] = matrix[i][j]+ min(matrix[i-1][j-1], matrix[i-1][j], matrix[i-1][j+1])
+                elif j == 0 :
+                    matrix[i][j] = matrix[i][j]+ min(matrix[i-1][j], matrix[i-1][j+1])
+                else :
+                    matrix[i][j] = matrix[i][j]+ min(matrix[i-1][j-1], matrix[i-1][j])
+        return min(matrix[m-1])
+        
