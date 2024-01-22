2e7161c62f9b2209762a1d3840b7a6e8b594fde9
Vivek M G <113228802+WiiWake3101@users.noreply.github.com>
2024-01-22 18:24:31 +0530

    Time: 127 ms (97.18%) | Memory: 13.4 MB (99.53%) - LeetSync

--- 645-set-mismatch/set-mismatch.py
+++ 645-set-mismatch/set-mismatch.py
@@ -4,13 +4,8 @@ class Solution(object):
         :type nums: List[int]
         :rtype: List[int]
         """
-        dup, missing = -1, -1
-        
-        for i in range(1, len(nums) + 1):
-            count = nums.count(i)
-            if count == 2:
-                dup = i
-            elif count == 0:
-                missing = i
-        
-        return [dup, missing]
+        n = len(nums)
+        a = sum(nums)
+        b = sum(set(nums))
+        s = n*(n+1)//2
+        return [a-b, s-b]
