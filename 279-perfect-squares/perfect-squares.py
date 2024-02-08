9fd9b2e97ec44ac96469118a3e840bee27e32418
Vivek M G <113228802+WiiWake3101@users.noreply.github.com>
2024-02-08 19:31:38 +0530

    Time: 107 ms (93.76%) | Memory: 17.6 MB (42.36%) - LeetSync

--- 279-perfect-squares/perfect-squares.py
+++ 279-perfect-squares/perfect-squares.py
@@ -1,12 +1,32 @@
 class Solution:
     def numSquares(self, n: int) -> int:
-        dp = [float('inf')] * (n + 1)
-        dp[0] = 0
-        for i in range(1, n + 1):
-            min_val = float('inf')
-            j = 1
-            while j * j <= i:
-                min_val = min(min_val, dp[i - j * j] + 1)
-                j += 1
-            dp[i] = min_val
-        return dp[n]
+        perfectSquares = [i**2 for i in range(1, math.floor(math.sqrt(n) + 1))]
+        def isPerfectSquare(num: int) -> bool:
+            temp = math.floor(math.sqrt(num))
+            return temp ** 2 == num
+
+        # print(perfectSquares)
+        
+        if isPerfectSquare(n):
+            return 1
+
+        candidates = set()
+        for item in perfectSquares:
+            candidates.add(n - item)
+        ans = 1
+
+        while candidates:
+            # print(candidates)
+            nextCandidates = set()
+            for candidate in candidates:
+                if isPerfectSquare(candidate):
+                    return ans + 1
+                for item2 in perfectSquares:
+                    if item2 > candidate:
+                        break
+                    if item2 not in nextCandidates:
+                        nextCandidates.add(candidate - item2)
+            candidates = nextCandidates
+            ans += 1
+        
+        return ans
