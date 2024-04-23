e77178106a3531816c7e65e055bd484ee2942e7b
Vivek M G <113228802+WiiWake3101@users.noreply.github.com>
2024-04-23 18:30:41 +0530

    Time: 19 ms (48.00%) | Memory: 17.7 MB (16.03%) - LeetSync

--- 23-merge-k-sorted-lists/merge-k-sorted-lists.cpp
+++ 23-merge-k-sorted-lists/merge-k-sorted-lists.cpp
@@ -8,45 +8,48 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
 class Solution {
 public:
     ListNode* mergeKLists(vector<ListNode*>& lists) {
-        if (lists.empty()) {
-            return nullptr;
+        vector<int> k;
+        int len=lists.size()-1;
+        if(lists.size()==0){
+            return NULL;
         }
-        return mergeKListsHelper(lists, 0, lists.size() - 1);
-    }
-    
-    ListNode* mergeKListsHelper(vector<ListNode*>& lists, int start, int end) {
-        if (start == end) {
-            return lists[start];
-        }
-        if (start + 1 == end) {
-            return merge(lists[start], lists[end]);
-        }
-        int mid = start + (end - start) / 2;
-        ListNode* left = mergeKListsHelper(lists, start, mid);
-        ListNode* right = mergeKListsHelper(lists, mid + 1, end);
-        return merge(left, right);
-    }
-    
-    ListNode* merge(ListNode* l1, ListNode* l2) {
-        ListNode* dummy = new ListNode(0);
-        ListNode* curr = dummy;
-        
-        while (l1 && l2) {
-            if (l1->val < l2->val) {
-                curr->next = l1;
-                l1 = l1->next;
-            } else {
-                curr->next = l2;
-                l2 = l2->next;
+        for(int i=0 ;i<=len;i++){
+            ListNode* p=lists[i];
+            if(p==NULL){
+                continue;
             }
-            curr = curr->next;
+            while(p!=NULL){
+                k.push_back(p->val);
+                p=p->next;
+            }
         }
-        
-        curr->next = l1 ? l1 : l2;
-        
-        return dummy->next;
+        sort(k.begin(),k.end());
+        if(k.size()==0){
+            return NULL;
+        }
+        ListNode* ans= new ListNode();
+        ListNode* curr=ans;
+        int length=k.size();
+        for(int i=0;i<length;i++){
+            curr->val=k[i];
+            if(i<length-1){
+                curr->next=new ListNode();
+                curr=curr->next;
+            }
+        }
+        return ans;
     }
 };
