5074510d2df58def4fc0ef752c19fe86e9dc32ea
Vivek M G <113228802+WiiWake3101@users.noreply.github.com>
2024-06-09 19:42:27 +0530

    Time: 75 ms (40.77%) | Memory: 57.4 MB (63.90%) - LeetSync

---
+++ 53-maximum-subarray/maximum-subarray.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var maxSubArray = function(nums) {
+    let maxSoFar = nums[0];
+    let currentMax = nums[0];
+
+    for (let i = 1; i < nums.length; i++) {
+        currentMax = Math.max(nums[i], currentMax + nums[i]);
+        maxSoFar = Math.max(maxSoFar, currentMax);
+    }
+
+    return maxSoFar;
+};
