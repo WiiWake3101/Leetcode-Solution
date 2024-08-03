7634c46d9fe33ae27e15f0291f7efca0e09c595b
Vivek M G <113228802+WiiWake3101@users.noreply.github.com>
2024-08-03 17:19:25 +0530

    Time: 51 ms (70.56%) | Memory: 48.6 MB (83.12%) - LeetSync

--- 75-sort-colors/sort-colors.js
+++ 75-sort-colors/sort-colors.js
@@ -3,18 +3,23 @@
  * @return {void} Do not return anything, modify nums in-place instead.
  */
 var sortColors = function(nums) {
-    let low = 0, mid = 0, high = nums.length - 1;
-
+ let low = 0
+    let mid = 0;
+    let high = nums.length - 1
     while (mid <= high) {
         if (nums[mid] === 0) {
-            [nums[low], nums[mid]] = [nums[mid], nums[low]];
-            low++;
-            mid++;
+            let temp = nums[low]
+            nums[low] = nums[mid]
+            nums[mid] = temp;
+            mid++
+            low++
         } else if (nums[mid] === 1) {
-            mid++;
-        } else {
-            [nums[mid], nums[high]] = [nums[high], nums[mid]];
-            high--;
+            mid++
+        } else if (nums[mid] === 2) {
+            let temp = nums[mid]
+            nums[mid] = nums[high]
+            nums[high] = temp
+            high--
         }
     }
 };
