dadbc5f36e92e0a12dee354d4668e7555e57e741
Vivek M G <113228802+WiiWake3101@users.noreply.github.com>
2024-08-03 17:28:11 +0530

    Time: 103 ms (59.63%) | Memory: 67.1 MB (12.38%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.js
+++ 238-product-of-array-except-self/product-of-array-except-self.js
@@ -3,23 +3,16 @@
  * @return {number[]}
  */
 var productExceptSelf = function(nums) {
-    const n = nums.length;
-    
-    const prefix = new Array(n).fill(1);
-    const suffix = new Array(n).fill(1);
-    
-    for (let i = 1; i < n; i++) {
-        prefix[i] = prefix[i - 1] * nums[i - 1];
-    }
-    
-    for (let i = n - 2; i >= 0; i--) {
-        suffix[i] = suffix[i + 1] * nums[i + 1];
-    }
-    
-    const answer = [];
-    for (let i = 0; i < n; i++) {
-        answer[i] = prefix[i] * suffix[i];
-    }
-    
-    return answer;
+  const answer = new Array(nums.length)
+  let leftProduct = 1
+  for (let i = 0; i < nums.length; i++) {
+    answer[i] = leftProduct
+    leftProduct *= nums[i]
+  }
+  let rightProduct = 1
+  for (let i = nums.length - 1; i >= 0; i--) {
+    answer[i] *= rightProduct
+    rightProduct *= nums[i]
+  }
+  return answer
 };
