8710d6a4acdeeec159757d5dbbe03fbd86c2c14b
Vivek M G <113228802+WiiWake3101@users.noreply.github.com>
2024-08-03 17:07:36 +0530

    Time: 45 ms (92.84%) | Memory: 49.9 MB (20.69%) - LeetSync

--- 961-long-pressed-name/long-pressed-name.js
+++ 961-long-pressed-name/long-pressed-name.js
@@ -4,14 +4,26 @@
  * @return {boolean}
  */
 var isLongPressedName = function(name, typed) {
-    let i = 0, j = 0;
-    while (j < typed.length) {
-        if (i < name.length && name[i] === typed[j]) {
+    let i = 0;
+    let j = 0;
+    const nlen = name.length;
+    const tlen = typed.length;
+
+    while (i < nlen && j < tlen) {
+        if (name[i] === typed[j]) {
             i++;
-        } else if (j === 0 || typed[j] !== typed[j - 1]) {
+            j++;
+        } else if (j > 0 && typed[j] === typed[j - 1]) {
+            j++;
+        } else {
             return false;
         }
+    }
+
+   
+    while (j < tlen && typed[j] === name[nlen - 1]) {
         j++;
     }
-    return i === name.length;
+
+    return i === nlen && j === tlen;
 };
