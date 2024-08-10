4b759d364454fd3acf5e0a5c24b6ac317bcba3db
Vivek M G <113228802+WiiWake3101@users.noreply.github.com>
2024-08-10 19:08:43 +0530

    Time: 63 ms (92.66%) | Memory: 56.4 MB (89.91%) - LeetSync

--- 41-first-missing-positive/first-missing-positive.js
+++ 41-first-missing-positive/first-missing-positive.js
@@ -7,20 +7,16 @@ var firstMissingPositive = function(nums) {
     
     for (let i = 0; i < n; i++) {
         while (nums[i] > 0 && nums[i] <= n && nums[nums[i] - 1] !== nums[i]) {
-            // Swap nums[i] with nums[nums[i] - 1]
+
             const temp = nums[nums[i] - 1];
             nums[nums[i] - 1] = nums[i];
             nums[i] = temp;
         }
     }
-    
-    // The first index that doesn't have i + 1 will be our answer
     for (let i = 0; i < n; i++) {
         if (nums[i] !== i + 1) {
             return i + 1;
         }
     }
-    
-    // If all numbers are in the correct place, return n + 1
     return n + 1;
 };
