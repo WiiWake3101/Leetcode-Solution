75a01a1cae0bb9af3b67b36f6e7cbfd5d3a351ba
Vivek M G <113228802+WiiWake3101@users.noreply.github.com>
2024-08-10 19:11:14 +0530

    Time: 59 ms (92.99%) | Memory: 53.3 MB (37.03%) - LeetSync

--- 51-n-queens/n-queens.js
+++ 51-n-queens/n-queens.js
@@ -2,27 +2,33 @@
  * @param {number} n
  * @return {string[][]}
  */
-var solveNQueens = function(N) {
-    let ans = [],
-        board = Array.from({length: N}, () => new Array(N).fill('.'))
-    
-    const place = (i, vert, ldiag, rdiag) => {
-        if (i === N) {
-            let res = new Array(N)
-            for (let row = 0; row < N; row++)
-                res[row] = board[row].join("")
-            ans.push(res)
-            return
-        }
-        for (let j = 0; j < N; j++) {
-            let vmask = 1 << j, lmask = 1 << (i+j), rmask = 1 << (N-i-1+j)
-            if (vert & vmask || ldiag & lmask || rdiag & rmask) continue
-            board[i][j] = 'Q'
-            place(i+1, vert | vmask, ldiag | lmask, rdiag | rmask)
-            board[i][j] = '.'
-        }
-    }
+var solveNQueens = function(n) {
+     const solutions = [];
+    const board = Array.from({ length: n }, () => Array(n).fill('.'));
 
-    place(0,0,0,0)
-    return ans
+    const isValid = (row, col) => {
+        for (let i = 0; i < row; i++) {
+            if (board[i][col] === 'Q') return false;
+            if (col - (row - i) >= 0 && board[i][col - (row - i)] === 'Q') return false;
+            if (col + (row - i) < n && board[i][col + (row - i)] === 'Q') return false;
+        }
+        return true;
+    };
+
+    const solve = (row) => {
+        if (row === n) {
+            solutions.push(board.map(row => row.join('')));
+            return;
+        }
+        for (let col = 0; col < n; col++) {
+            if (isValid(row, col)) {
+                board[row][col] = 'Q';
+                solve(row + 1);
+                board[row][col] = '.';
+            }
+        }
+    };
+
+    solve(0);
+    return solutions;
 };
