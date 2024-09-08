46e4073550a91849983e95889bd9ad2eef13d032
Vivek M G <113228802+WiiWake3101@users.noreply.github.com>
2024-09-08 14:44:59 +0530

    Time: 61 ms (21.43%) | Memory: 49.5 MB (36.66%) - LeetSync

--- 62-unique-paths/unique-paths.js
+++ 62-unique-paths/unique-paths.js
@@ -4,17 +4,12 @@
  * @return {number}
  */
 var uniquePaths = function(m, n) {
-    let dp = new Array(m).fill().map(() => new Array(n).fill(0));
-    for (let i = 0; i < m; i++) {
-        dp[i][n-1] = 1;
-    }
-    for (let j = 0; j < n; j++) {
-        dp[m-1][j] = 1;
-    }
-    for (let i = m - 2; i >= 0; i--) {
-        for (let j = n - 2; j >= 0; j--) {
-            dp[i][j] = dp[i+1][j] + dp[i][j+1];
+      const dp = Array(m).fill().map(() => Array(n).fill(1));
+
+    for(let i=1;i<m;i++){
+        for(let j=1;j<n;j++){
+            dp[i][j] = dp[i-1][j] + dp[i][j-1]
         }
     }
-    return dp[0][0];
+    return dp[m-1][n-1];
 };
